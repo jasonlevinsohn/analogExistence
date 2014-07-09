b6a2d40c8ab28b0d309a7c7d5ce5371da963e7a0
Jason Levinsohn <jason.levinsohn@gmail.com>
2014-07-09 12:33:56 -0400

    added Build configuration

---
+++ build.config.js
@@ -0,0 +1,13 @@
+/**
+ * This module contains all the configuration for the build process
+ */
+module.exports = {
+    /**
+     * dev_dir Directory is where our projects are compiled for development.
+     * prod_dir Directory is where our projects are compiled.
+     */
+    src_dir: 'src',
+    dev_dir: 'build',
+    prod_dir: 'bin'
+
+};
